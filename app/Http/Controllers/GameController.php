0ddee9732e6e40e1b2a47c99b8228526e22c05a1
Chris <cbrosnan00@gmail.com>
2026-08-20 20:10:23 +0100

    Update GameController.php

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -9,6 +9,7 @@ use App\Models\Game;
 use App\Models\Location;
 use App\Models\Scene;
 use App\Models\System;
+use App\Models\Attachment;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -43,6 +44,10 @@ class GameController extends Controller
         $game = Game::find($game_id);
 
         Gate::authorize('view', $game);
+
+        $thumbnail = Attachment::where('id', $game->thumbnail_id)->where('user_id', auth()->id())->first() ?? null;
+
+        $game->thumbnail = $thumbnail;
 
         return inertia('games_view', [
             'game' => $game,
@@ -103,7 +108,9 @@ class GameController extends Controller
         $this->storeObjectAttachments($request, $game, 'game');
         $game->save();
 
-        return response()->json($game, 201);
+        $thumbnail = Attachment::where('id', $game->thumbnail_id)->where('user_id', auth()->id())->first() ?? null;
+
+        return redirect()->route('games.show', ['game_id' => $game->id, 'thumbnail' => $thumbnail])->with('success', 'Game created successfully.');
     }
 
     public function update(Request $request, $game_id): RedirectResponse|JsonResponse
