88aa5c6c87bdbea53914c4d0e40b48090938f142
Chris <cbrosnan00@gmail.com>
2026-09-03 16:07:32 +0100

    Thumbnails

--- resources/js/pages/games_view.tsx
+++ resources/js/pages/games_view.tsx
@@ -63,7 +63,7 @@ export default function GamesView({
                 <img
                     src={
                         game?.thumbnail?.attachment_path
-                            ? `/storage/${game.thumbnail.attachment_path}`
+                            ? `/uploads/games/${game.thumbnail.attachment_path}`
                             : '/images/default-thumbnail.svg'
                     }
                     alt="Game Thumbnail"
--- resources/js/pages/locations_view.tsx
+++ resources/js/pages/locations_view.tsx
@@ -70,7 +70,7 @@ export default function Locations({
                 <img
                     src={
                         location?.thumbnail?.attachment_path
-                            ? `/storage/${location.thumbnail.attachment_path}`
+                            ? `/uploads/locations/${location.thumbnail.attachment_path}`
                             : '/images/default-thumbnail.svg'
                     }
                     alt="Location thumbnail"
