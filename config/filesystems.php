46069e4c23aa3152fd8e6ee94e7c67d3e0f030f0
Chris <cbrosnan00@gmail.com>
2026-09-04 08:51:07 +0100

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,6 +58,7 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => true,
             'report' => true,
+            'visibility' => 'public',
         ],
 
     ],
