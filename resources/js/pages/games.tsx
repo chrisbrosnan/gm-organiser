401576a287090595c32b20ebbf236e7cf2386b84
Chris <cbrosnan00@gmail.com>
2026-09-16 09:00:11 +0100

    Update games.tsx

--- resources/js/pages/games.tsx
+++ resources/js/pages/games.tsx
@@ -99,7 +99,7 @@ export default function Games({ auth, games, bucketUrl }: { auth: { user: { id: 
                                             : '/images/default-thumbnail.svg'
                                     }
                                     alt="Game Thumbnail"
-                                    className="mb-4 h-32 w-64 rounded-lg object-cover"
+                                    className="mb-4 h-46 w-full rounded-lg object-cover"
                                 />
                                 <h2 className="text-xl font-bold">{game.name}</h2>
                                 <p>System: {game.system_name}</p>
